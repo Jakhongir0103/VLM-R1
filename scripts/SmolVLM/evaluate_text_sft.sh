7df3cd30995e2bc2187dd132bc24dd602c18a821
Sébastien Delsad <seb.ge.ed@gmail.com>
2025-05-29 19:11:45 +0200

    Final Results

--- scripts/SmolVLM/evaluate_text_sft.sh
+++ scripts/SmolVLM/evaluate_text_sft.sh
@@ -2,14 +2,14 @@
 cd $REPO/src/open-r1-multimodal/src/open_r1/
 
 MODEL_DIR=/scratch/izar/delsad/vlm_r1/output
+MODEL_DIR2=/scratch/izar/delsad/vlm_r1_text_bias/output
+
+python -u evaluate_SmolVLM.py \
+    --model_path $MODEL_DIR2/SmolVLM-2B-Instruct-SFT/merged \
+    --input_data_dir $DATA_PATH/vsr \
+    --output_data_dir /home/delsad/VI/VLM-R1/results/vsr/SmolVLM/sft-strongtextbias-all
 
 python -u evaluate_SmolVLM.py \
     --model_path $MODEL_DIR/SmolVLM-2B-Instruct-SFT/merged \
     --input_data_dir $DATA_PATH/vsr \
-    --output_data_dir /home/delsad/VI/VLM-R1/results/vsr/SmolVLM/sft-val
-
-python -u evaluate_SmolVLM.py \
-    --model_path $MODEL_DIR/SmolVLM-2B-Instruct-SFT/merged \
-    --input_data_dir $DATA_PATH/vsr \
-    --output_data_dir /home/delsad/VI/VLM-R1/results/vsr/SmolVLM/sft-test \
-    --use_test_set
+    --output_data_dir /home/delsad/VI/VLM-R1/results/vsr/SmolVLM/sft-all
